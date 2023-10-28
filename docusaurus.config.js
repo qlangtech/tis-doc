84607bea597a21fe7ac9554d4a0d063280bb9516
mozhenghua <baisui@qlangtech.com>
2023-10-28 14:33:50 +0800

    add aliyun jindofs relvant

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ const config = {
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    includeCurrentVersion: false,
+                    includeCurrentVersion: true,
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.

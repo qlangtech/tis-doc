92bcfb647e3acd40f082ed42c7064425796f4253
mozhenghua <baisui@qlangtech.com>
2024-05-21 11:32:52 +0800

    release v4.0.0

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,7 +5,31 @@ import Link from '@docusaurus/Link';
 import CodeBlock from '@theme/CodeBlock';
 import Figure from '/src/components/Figure';
 
-export default function Contact() {
+export default function Contact({children, justGongZhongHao}) {
+
+    if (justGongZhongHao) {
+        return (
+            <table style={{width: '100%', display: 'table'}}>
+                <tr>
+                    <td>
+                        {children}
+                    </td>
+                    <td>
+
+                        <div className="row">
+                            <div className={clsx('col col-12 text--center')}>
+                                <h3>微信公众号</h3>
+                                <div style={{color: '#666666'}}>定期发布最新TIS功能特性博客</div>
+                                <div><img src={require('/img/weixin_talk_group.jpg').default} width="250"/></div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        );
+    }
+
+
     return (
         <div className="container contact">
             <div className="row">
@@ -14,13 +38,13 @@ export default function Contact() {
                 </div>
                 <div className={clsx('col col--4 text--center')}>
                     <h3>微信TIS小秘书</h3>
-                    <div style={{color:'#666666'}}>添加后会邀请您进入微信群参与讨论</div>
+                    <div style={{color: '#666666'}}>添加后会邀请您进入微信群参与讨论</div>
                     <div>
-                        <img src={require('/img/weixin.jpeg').default}     width="250"/></div>
+                        <img src={require('/img/weixin.jpeg').default} width="250"/></div>
                 </div>
                 <div className={clsx('col col--4 text--center')}>
                     <h3>微信公众号</h3>
-                    <div style={{color:'#666666'}}>定期发布最新TIS功能特性博客</div>
+                    <div style={{color: '#666666'}}>定期发布最新TIS功能特性博客</div>
                     <div><img src={require('/img/weixin_talk_group.jpg').default} width="250"/></div>
                 </div>
             </div>

157c0345fb667470c6a661ff34efe6a6a0583aa7
mozhenghua <baisui@qlangtech.com>
2025-08-24 11:38:16 +0800

    add rate-controller

--- update_remote.sh
+++ update_remote.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 # hugo mod clean
 ansible-playbook  ./deploy.yml -i ./inventory/hosts
-#rsync  -vr ./build/ root@59.110.27.59:/usr/share/tomcat/webapps/ROOT
+#rsync --delete  -vr ./build/ root@59.110.27.59:/usr/share/tomcat/webapps/ROOT
 

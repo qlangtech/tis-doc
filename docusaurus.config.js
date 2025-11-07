f2a6aab617915433682ef3dfda146f6cafb4d594
baisui1981 <baisui@qlangtech.com>
2025-11-07 13:59:25 +0800

    set includeCurrentVersion to false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
-                    includeCurrentVersion: true,
+                    includeCurrentVersion: false,
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
